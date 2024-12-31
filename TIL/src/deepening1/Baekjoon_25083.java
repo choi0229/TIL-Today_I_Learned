6d6fac702b75f48b49a37858adea687c91fa4ae4
choi0229 <elcax29@gmail.com>
2025-01-01 01:36:30 +0900

    250101
    단계별로 풀어보기 25083번 브론즈5레벨

---
+++ TIL/src/deepening1/Baekjoon_25083.java
@@ -0,0 +1,14 @@
+package deepening1;
+
+public class Baekjoon_25083 {
+
+	public static void main(String[] args) {
+		// 새싹 출력하기
+		System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+	}
+}
