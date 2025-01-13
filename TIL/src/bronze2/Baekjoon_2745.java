437b51cfd510c7c407a2c5cc051e8d53f962de9f
choi0229 <elcax29@gmail.com>
2025-01-13 23:05:34 +0900

    250113
    백준단계별로 풀어보기 일반수학1

---
+++ TIL/src/bronze2/Baekjoon_2745.java
@@ -0,0 +1,15 @@
+package bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_2745 {
+	public static void main(String[] args)throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] s1 = bf.readLine().split(" ");
+        int n = Integer.parseInt(s1[1]);
+        System.out.println(Integer.parseInt(s1[0],n));
+    }
+}
