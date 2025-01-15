2a25453d578590cb800582d005d0e2a861d77452
choi0229 <elcax29@gmail.com>
2025-01-15 23:34:21 +0900

    250115
    단계별로 풀어보기 1문제와 C# 알고리즘 공부 기초

---
+++ C#/DotNet/DotNet/Algorithms/SumAlgorithm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class SumAlgorithm
+{
+
+    static void Main()
+    {
+        // input : n명의 국어 점수
+        int[] scores = { 100, 75, 50, 37, 90, 95 };
+        int sum = 0;
+
+        // process : 합계 알고리즘
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] >= 80)
+            {
+                sum += scores[i];
+            }
+        }
+
+        // output
+        Console.WriteLine($"{scores.Length}명의 점수 중 80점 이상의 총점 : {sum}");
+
+    }
+}
---
+++ C#/programmers/programmers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+// 배열의 평균값
+/*
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]                 5.5
+[89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]    94.0
+*/
+public class Lv0_1
+{
+    public double solution(int[] numbers)
+    {
+        double answer = 0;
+        int count = numbers.Length;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            answer += numbers[i];
+        }
+        answer /= (double)count;
+        return answer;
+    }
+}
+
+
+// 짝수 홀수 개수
+/*
+[1, 2, 3, 4, 5] [2, 3]
+[1, 3, 5, 7]    [0, 4]
+*/
+public class Lv0_2
+{
+    public int[] solution(int[] num_list)
+    {
+        int[] answer = new int[2];
+
+        for (int i = 0; i < num_list.Length; i++)
+        {
+            if (num_list[i] % 2 == 0)
+            {
+                answer[0]++;
+            }
+            else
+            {
+                answer[1]++;
+            }
+        }
+        return answer;
+    }
+}
+
